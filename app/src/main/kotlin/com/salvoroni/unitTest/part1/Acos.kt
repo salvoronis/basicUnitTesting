0e4973edcafba2c3bd71732a2c980db0126fcec8
salvoronis <salvoronis@gmail.com>
2021-03-02 15:45:10 +0300

    Shell sort

--- app/src/main/kotlin/com/salvoroni/unitTest/part1/Acos.kt
+++ app/src/main/kotlin/com/salvoroni/unitTest/part1/Acos.kt
@@ -1,13 +1,21 @@
 package com.salvoroni.unitTest.part1
 
 import kotlin.math.*;
+import com.salvoroni.unitTest.part2.shellSort
 
 fun _acos(x: Double): Double {
     val odd_sequence: (Int) -> Int = {(1..it step 2).fold(1) { prew, i -> prew * i }}
     val denominator: (Int) -> Int = {(2..it step 2).fold(1) { prew, i -> prew * i * (i+1) }}
-    return PI/2 - (x + generateSequence(1) { n -> n + 1 }.take(10).map { n -> (odd_sequence(n) * x.pow(2*n+1)) / (denominator(n)) }.sum())
+    return PI/2 - (x + generateSequence(1) { n -> n + 1 }.take(7).map { n -> (odd_sequence(n) * x.pow(2*n+1)) / (denominator(n)) }.sum())
 }
 
 fun main() {
-    println(_acos(0.0))
+    println(_acos(-0.5))
+    val arr = intArrayOf(23, 12, 1,8, 34, 54, 2, 3)
+    for (i in arr) print("$i ")
+
+    shellSort(arr)
+    println()
+
+    for (i in arr) print("$i ")
 }
---
+++ app/src/main/kotlin/com/salvoroni/unitTest/part2/ShellSort.kt
@@ -0,0 +1,21 @@
+package com.salvoroni.unitTest.part2
+
+fun shellSort(arr: IntArray) {
+    val n = arr.size
+    var gap = n/2
+    
+    while(gap > 0) {
+        var i = gap
+        while(i < n) {
+            val temp = arr[i]
+            var j = i
+            while(j >= gap && arr[j-gap] > temp) {
+                arr[j] = arr[j-gap]
+                j -= gap
+            }
+            arr[j] = temp
+            i += 1
+        }
+        gap /= 2
+    }
+}
